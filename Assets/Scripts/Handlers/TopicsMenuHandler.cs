55b5c8c7dbeff55ab0f7bfa659c71eea0eb6ac9b
ThomasNeyman <neymanthomas45@gmail.com>
2022-04-09 23:26:38 -0600

    Many UI updates

--- Assets/Scripts/Handlers/TopicsMenuHandler.cs
+++ Assets/Scripts/Handlers/TopicsMenuHandler.cs
@@ -6,9 +6,63 @@ using TMPro;
 
 public class TopicsMenuHandler : MonoBehaviour
 {
+    [SerializeField] private TMP_Dropdown ChapterSelect;
+    [SerializeField] private TMP_Dropdown TopicSelect;
+    [SerializeField] private TMP_Text ModeButtonText;
+    public static Chapter SelectedChapter;
+    public static Topic SelectedTopic;
+    public static bool JapaneseToEnglish;
+
+    public enum Chapter {
+        All,
+        Chapter_1,
+        Chapter_2,
+        Chapter_3,
+        Chapter_4,
+        Chapter_5,
+        Chapter_6
+    }
+
+    public enum Topic {
+        All,
+        Nouns,
+        Verbs,
+        Adjectives,
+        Adverbs
+    }
+
+    private void Start() {
+        SelectedChapter = Chapter.All;
+        SelectedTopic = Topic.All;
+        JapaneseToEnglish = true;
+        ChapterSelect.value = (int)SelectedChapter;
+        TopicSelect.value = (int)SelectedTopic;
+    }
+
     public void LoadJapaneseToEnglish() {
+        if (validateSelection()) {
+            SelectedChapter = (Chapter)ChapterSelect.value;
+            SelectedTopic = (Topic)TopicSelect.value;
+            SoundManager.instance.Play("Bloop 1");
+            SceneManager.LoadScene("VocabularyScene");
+        } else {
+            SoundManager.instance.Play("Bloop 4");
+        }
+    }
+
+    public void ChangeMode() {
         SoundManager.instance.Play("Bloop 1");
-        SceneManager.LoadScene("VocabularyScene");
+        if (JapaneseToEnglish) {
+            JapaneseToEnglish = false;
+            ModeButtonText.text = "ENG to JPN";
+        } else {
+            JapaneseToEnglish = true;
+            ModeButtonText.text = "JPN to ENG";
+        }
+    }
+
+    private bool validateSelection() {
+        return true;
     }
 
     public void BackToMain() {
--- Assets/Scripts/Handlers/VocabularySceneHandler.cs
+++ Assets/Scripts/Handlers/VocabularySceneHandler.cs
@@ -13,21 +13,59 @@ public class VocabularySceneHandler : MonoBehaviour
     [SerializeField] private TMP_Text PronunciationText;
     private System.Random rand;
     private int currentIndex;
+    private int chapterIndexStart, chapterIndexEnd;
 
     private void Start()
     {
         rand = new System.Random();
+        init();
         AnswerText.text = "";
         PronunciationText.text = "";
-        currentIndex = rand.Next(0, JapaneseDictionaries.JapaneseToEnglish.Count);
+        currentIndex = rand.Next(chapterIndexStart, chapterIndexEnd);
         QuestionText.text = JapaneseDictionaries.JapaneseToEnglish.ElementAt(currentIndex).Key;
+    }
+
+    private void init() {
+        // Sets the limits on what the random number generator can generate based
+        // on what the user selected for a chapter. The full dictionary is used
+        // if all chapters option was selected.
+        switch(TopicsMenuHandler.SelectedChapter) {
+            case TopicsMenuHandler.Chapter.All:
+                chapterIndexStart = 0;
+                chapterIndexEnd = JapaneseDictionaries.JapaneseToEnglish.Count;
+                break;
+            case TopicsMenuHandler.Chapter.Chapter_1:
+                chapterIndexStart = 0;
+                chapterIndexEnd = 138;
+                break;
+            case TopicsMenuHandler.Chapter.Chapter_2:
+                chapterIndexStart = 138;
+                chapterIndexEnd = 209;
+                break;
+            case TopicsMenuHandler.Chapter.Chapter_3:
+                chapterIndexStart = 209;
+                chapterIndexEnd = 263;
+                break;
+            case TopicsMenuHandler.Chapter.Chapter_4:
+                chapterIndexStart = 263;
+                chapterIndexEnd = 351;
+                break;
+            case TopicsMenuHandler.Chapter.Chapter_5:
+                chapterIndexStart = 351;
+                chapterIndexEnd = 405;
+                break;
+            case TopicsMenuHandler.Chapter.Chapter_6:
+                chapterIndexStart = 0;
+                chapterIndexEnd = 1;
+                break;
+        }
     }
 
     public void NextQuestion() {
         SoundManager.instance.Play("Bloop 1");
         AnswerText.text = "";
         PronunciationText.text = "";
-        currentIndex = rand.Next(0, JapaneseDictionaries.JapaneseToEnglish.Count);
+        currentIndex = rand.Next(chapterIndexStart, chapterIndexEnd);
         QuestionText.text = JapaneseDictionaries.JapaneseToEnglish.ElementAt(currentIndex).Key;
     }
 
